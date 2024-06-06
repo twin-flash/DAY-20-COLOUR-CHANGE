4a9c3bc4c29b032e0c9c2858f9648f9cecd54ab8
Mohan kumar S <itsmohankumar24@gmail.com>
2024-06-06 17:19:03 +0530

    JAVASCRIPT FOLDER CHANGE

--- java.js
+++ java.js
@@ -1,1 +1,9 @@
-
+var box = document.getElementById("title-line")
+function mode_red() {
+  box.style.color ="red"
+  console.log(box)
+}
+function mode_orange() {
+  box.style.color ="rgb(220,95,0)"
+  console.log(box)
+}
